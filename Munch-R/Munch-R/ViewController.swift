c0919a363a5e7732edfa877b246a3d2451cdbf3c
gilr00y <rhombe@jasonrhombesMBP.home>
2019-07-03 21:05:13 -0400

    Create XCode project.

---
+++ Munch-R/Munch-R/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Munch-R
+//
+//  Created by jason@rhombe on 7/3/19.
+//  Copyright © 2019 Rhombe Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
